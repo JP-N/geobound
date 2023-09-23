b06e248e5b981a03fd00e52b124480c0c1eeb532
rvalle <rogelio-valle@uiowa.edu>
2023-09-23 10:19:23 -0500

    ahh

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// TILE 38 Connection
+var Tile38 = require('tile38');
+var client = new Tile38();
+
+// save a location in format (group, key, cords)
+client.set('UIowa', 'IMU', [41.6631103,-91.5383735]); // cords for the IMU for testing
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
