4b81ebd649c6f932ea06fc74a3ee01df89baaa56
jpnoga <jpnogax@gmail.com>
2023-09-24 12:07:30 -0500

    Latest changes

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH_SECRET,
-  returnTo: 'http://geobound.tech',
-  baseURL: 'http://geobound.tech',
+  baseURL: 'http://localhost:3000',
   clientID: process.env.AUTH_CLIENT_ID,
   issuerBaseURL: process.env.AUTH_ISSUER_BASE_URL
 };
